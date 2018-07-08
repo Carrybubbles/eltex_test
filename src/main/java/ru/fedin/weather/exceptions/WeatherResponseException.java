0a9ef5790bd2039b9ac827bd4fab57fbb78e7f02
Artem Fedin <a.fedin@g.nsu.ru>
2018-07-08 23:33:13 +0700

    delete garbage
    delete garbage

--- src/main/java/ru/fedin/weather/exceptions/WeatherResponseException.java
+++
@@ -1,8 +0,0 @@
-package ru.fedin.weather.exceptions;
-
-public class WeatherResponseException extends Exception {
-    public WeatherResponseException() { super(); }
-    public WeatherResponseException(String message) { super(message); }
-    public WeatherResponseException(String message, Throwable cause) { super(message, cause); }
-    public WeatherResponseException(Throwable cause) { super(cause); }
-}
